0e7eb9bbdf96dbda2adc5c433cd9af59aa711a73
Sun3-11 <shamsalthri@gmail.com>
2025-01-23 05:06:39 +0300

    :)

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import heroImg from "../assets/hero.svg";
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Never forget that your journey is unique to you. While others may
+            walk different paths, you are on your own journey of self-discovery
+            and growth. The obstacles you face are part of the story that will
+            make you stronger. No one else can walk your path for you, and no
+            one else can define your success. Only you have the power to create
+            the life you want.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+const projects = [
+  {
+    title: "games list",
+    url: "https://games-list-livid.vercel.app/",
+    image: "./assets/game.png",
+  },
+  {
+    title: "birthday buddy",
+    url: "https://birthday-buddy-ashy.vercel.app/",
+    image: "./assets/birthday.png",
+  },
+  {
+    title: "tours",
+    url: "https://tours-snowy.vercel.app/",
+    image: "./assets/tours.png",
+  },
+];
